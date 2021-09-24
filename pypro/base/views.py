05dfd60d335c3b4b06ffe73e3f0f290188ffc3c9
rafael-hsm <rafameireles2011@gmail.com>
2021-09-24 10:50:28 -0300

    Setting Django Toolbar.

--- pypro/base/views.py
+++ pypro/base/views.py
@@ -5,4 +5,7 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse('<html><body>Olá Django</body></html>', content_type='text/html')
+    return HttpResponse('<!DOCTYPE html><html lang="pt-br"><head><meta charset="UTF-8">'
+                        '<meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" '
+                        'content="width=device-width, initial-scale=1.0"><title>Módulo Django - DevPro</title>'
+                        '</head><body>''Olá Django</body></html>', content_type='text/html')
