b29f77df06be061b168e798fcbe1cae0af9ef290
rafael-hsm <rafameireles2011@gmail.com>
2021-10-04 22:53:45 -0300

    Session HTTP e templates.

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 
+videos = [
+    {'slug': 'motivacao', 'titulo': 'Vídeo Aperitivo: Motivação', 'vimeo_id': 621742384},
+    {'slug': 'instalacao-windows', 'titulo': 'Instalação Windows', 'vimeo_id': 251497668},
+]
+
+videos_dct = {dct['slug']: dct for dct in videos}
+
+
+def indice(request):
+    return render(request, 'aperitivos/indice.html', context={'videos': videos})
+
 
 def video(request, slug):
-    return render(request, 'aperitivos/video.html')
+    video = videos_dct[slug]
+    return render(request, 'aperitivos/video.html', context={'video': video})
