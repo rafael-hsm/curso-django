3cc8652af068569efe661e37f739d2e222f1c8a7
rafael-hsm <rafameireles2011@gmail.com>
2021-10-08 10:39:50 -0300

    Lesson_module_app_requirements

--- pypro/settings.py
+++ pypro/settings.py
@@ -45,7 +45,8 @@ INSTALLED_APPS = [
     'collectfast',
     'django.contrib.staticfiles',
     'pypro.base',
-    'pypro.aperitivos'
+    'pypro.aperitivos',
+    'pypro.modulos'
 ]
 
 MIDDLEWARE = [
