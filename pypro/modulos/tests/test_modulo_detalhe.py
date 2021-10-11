abee13a0446b5ff2d00f30900d8d5cedbff821f8
rafael-hsm <rafameireles2011@gmail.com>
2021-10-11 07:53:53 -0300

    Lesson_module_app_requirements

---
+++ pypro/modulos/tests/test_modulo_detalhe.py
@@ -0,0 +1,30 @@
+import pytest
+from django.urls import reverse
+
+from pypro.django_assertions import assert_contains
+from model_mommy import mommy
+
+from pypro.modulos.models import Modulo
+
+
+@pytest.fixture()
+def modulo(db):
+    return mommy.make(Modulo)
+
+
+@pytest.fixture
+def resp(client, modulo):
+    resp = client.get(reverse('modulos:detalhe', kwargs={'slug': modulo.slug}))
+    return resp
+
+
+def test_titulo(resp, modulo: Modulo):
+    assert_contains(resp, modulo.titulo)
+
+
+def test_descricao(resp, modulo: Modulo):
+    assert_contains(resp, modulo.descricao)
+
+
+def test_publico(resp, modulo: Modulo):
+    assert_contains(resp, modulo.publico)
