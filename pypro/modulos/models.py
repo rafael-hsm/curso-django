9d5976e302885299f67dce9f49d0769599ca4049
rafael-hsm <rafameireles2011@gmail.com>
2021-10-11 07:53:53 -0300

    Lesson_module_app_requirements

--- pypro/modulos/models.py
+++ pypro/modulos/models.py
@@ -1,3 +1,5 @@
+from django.urls.base import reverse
+
 from django.db import models
 from ordered_model.models import OrderedModel
 
@@ -6,9 +8,13 @@ class Modulo(OrderedModel):
     titulo = models.CharField(max_length=64)
     publico = models.TextField()
     descricao = models.TextField()
+    slug = models.SlugField(unique=True)
 
     class Meta(OrderedModel.Meta):
         pass
 
     def __str__(self):
         return self.titulo
+
+    def get_absolute_url(self):
+        return reverse('modulos:detalhe', kwargs={'slug': self.slug})
