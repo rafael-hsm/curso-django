c51945ab993f7259e82266be31f014129a3c9e36
rafael-hsm <rafameireles2011@gmail.com>
2021-10-17 17:12:17 -0300

    Fixing CI Travis erros.

--- pypro/turmas/admin.py
+++ pypro/turmas/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-from django.contrib.admin import TabularInline
 
 # Register your models here.
 
--- pypro/turmas/migrations/0002_criacao_matricula.py
+++ pypro/turmas/migrations/0002_criacao_matricula.py
@@ -6,7 +6,6 @@ import django.db.models.deletion
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
         ('turmas', '0001_initial'),
@@ -19,7 +18,8 @@ class Migration(migrations.Migration):
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('data', models.DateTimeField(auto_now_add=True)),
                 ('turma', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='turmas.turma')),
-                ('usuario', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
+                ('usuario', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE,
+                                              to=settings.AUTH_USER_MODEL)),
             ],
         ),
         migrations.AddField(
