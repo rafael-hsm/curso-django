900f4e72aa2c3f28c7b4c735835491c55be01bdd
rafael-hsm <rafameireles2011@gmail.com>
2021-10-18 20:08:01 -0300

    Fixing CI Travis erros.

--- pypro/settings.py
+++ pypro/settings.py
@@ -34,6 +34,10 @@ ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 AUTH_USER_MODEL = 'base.User'
 
+LOGIN_URL = '/contas/login/'
+LOGIN_REDIRECT_URL = '/modulos/'
+LOGOUT_REDIRECT_URL = '/'
+
 # Application definition
 
 INSTALLED_APPS = [
--- pypro/urls.py
+++ pypro/urls.py
@@ -17,9 +17,9 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('contas/', include('django.contrib.auth.urls')),
     path('', include('pypro.base.urls')),
     path('aperitivos/', include('pypro.aperitivos.urls')),
     path('modulos/', include('pypro.modulos.urls')),
@@ -28,6 +28,7 @@ urlpatterns = [
 
 if settings.DEBUG:
     import debug_toolbar
+
     urlpatterns.append(
         path('__debug__/', include(debug_toolbar.urls)),
     )
