b979c71c94cbb35cb0553c211e0977d2ec278c24
rafael-hsm <rafameireles2011@gmail.com>
2021-10-18 20:08:01 -0300

    Fixing CI Travis erros.

--- pypro/modulos/views.py
+++ pypro/modulos/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 from pypro.modulos import facade
 
@@ -14,6 +15,7 @@ def detalhe(request, slug):
     return render(request, 'modulos/modulo_detalhe.html', {'modulo': modulo, 'aulas': aulas})
 
 
+@login_required
 def aula(request, slug):
     aula = facade.encontrar_aula(slug)
     return render(request, 'modulos/aula_detalhe.html', {'aula': aula})
